b5adb1f01b5d66d774a9a66127ccda63f9c81e2c
Kirill Kholodilin <chrl@users.noreply.github.com>
2018-08-27 23:40:21 +0200

    Create example.go

---
+++ example.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/bottalk/go-plugin"
+
+func main() {
+
+	plugin := bottalk.NewPlugin()
+
+	plugin.Actions = []bottalk.Action{{
+		Name:        "Test action",
+		Description: "My description",
+		Endpoint:    "/hello",
+		Action: func() string {
+			return "ok"
+		},
+	}}
+
+	plugin.Run(":8080")
+}
